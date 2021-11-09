aa1594de9b36f3b8b62652fbd4f9553e8f686be3
KilCote <sacredPhoenix888@outlook.com>
2021-11-08 16:32:53 -0800

    fix cell refresh onDraw fix

--- src/com/kilcote/evocraft/views/game/cell/JavaGameCellUI.java
+++ src/com/kilcote/evocraft/views/game/cell/JavaGameCellUI.java
@@ -14,6 +14,13 @@ import javafx.scene.shape.Rectangle;
 public class JavaGameCellUI extends GameObjCellUI<JavaGameCellModel> {
 	
 	private boolean isDrawing = false;
+	
+	private Rectangle top;
+	private Rectangle bottom;
+	private Rectangle left;
+	private Rectangle right;
+	private Rectangle center;
+	
 	public JavaGameCellUI(JavaGameCellModel cell) {
 		setModel(cell);
 		cell.setUI(this);
@@ -29,45 +36,59 @@ public class JavaGameCellUI extends GameObjCellUI<JavaGameCellModel> {
 				this.shape.setBackground(new Background(new BackgroundFill(StandaloneSettings.roadBackground, CornerRadii.EMPTY, Insets.EMPTY)));
 			}
 		} else if (shape.getWidth() != 0 || shape.getHeight() != 0) {
-			Rectangle rect;
+//			Rectangle rect;
 			if (!isDrawing) {
 				if ((getModel().isOpenTop || getModel().isOpenBottom) && (getModel().isOpenLeft || getModel().isOpenRight)) {
-					rect = FormRect();
-					rect.setWidth(StandaloneSettings.roadWidth * shape.getWidth());
-					rect.setHeight(StandaloneSettings.roadHeight * shape.getHeight());
-					shape.getChildren().add(rect);
-					rect.setX(Math.round((shape.getWidth() - rect.getWidth()) / 2));
-					rect.setY(Math.round((shape.getHeight() - rect.getHeight()) / 2));
+					center = FormRect();
+					shape.getChildren().add(center);
 				}
 				if (getModel().isOpenTop) {
-					rect = FormRect();
-					rect.setWidth(StandaloneSettings.roadWidth * shape.getWidth());
-					rect.setX(Math.round((shape.getWidth() - rect.getWidth()) / 2));
-					rect.setY(0);
-					shape.getChildren().add(rect);
+					top = FormRect();
+					shape.getChildren().add(top);
 				}
 				if (getModel().isOpenBottom) {
-					rect = FormRect();
-					rect.setWidth(StandaloneSettings.roadWidth * shape.getWidth());
-					rect.setX(Math.round((shape.getWidth() - rect.getWidth()) / 2));
-					rect.setY(Math.round(shape.getHeight() / 2));
-					shape.getChildren().add(rect);
+					bottom = FormRect();
+					shape.getChildren().add(bottom);
 				}
 				if (getModel().isOpenLeft) {
-					rect = FormRect();
-					rect.setHeight(StandaloneSettings.roadHeight * shape.getHeight());
-					shape.getChildren().add(rect);
-					rect.setX(0);
-					rect.setY(Math.round(((shape.getHeight() - rect.getHeight()) / 2)));
+					left = FormRect();
+					shape.getChildren().add(left);
 				}
 				if (getModel().isOpenRight) {
-					rect = FormRect();
-					rect.setHeight(StandaloneSettings.roadHeight * shape.getHeight());
-					shape.getChildren().add(rect);
-					rect.setX(shape.getWidth() / 2);
-					rect.setY(Math.round((shape.getHeight() - rect.getHeight()) / 2));
+					right = FormRect();
+					shape.getChildren().add(right);
 				}
 				isDrawing = true;
+			}
+			if ((getModel().isOpenTop || getModel().isOpenBottom) && (getModel().isOpenLeft || getModel().isOpenRight)) {
+				center.setWidth(StandaloneSettings.roadWidth * shape.getWidth());
+				center.setHeight(StandaloneSettings.roadHeight * shape.getHeight());
+				center.setX(Math.round((shape.getWidth() - center.getWidth()) / 2));
+				center.setY(Math.round((shape.getHeight() - center.getHeight()) / 2));
+			}
+			if (getModel().isOpenTop) {
+				top.setWidth(StandaloneSettings.roadWidth * shape.getWidth());
+				top.setHeight(Math.round(shape.getHeight() / 2));
+				top.setX(Math.round((shape.getWidth() - top.getWidth()) / 2));
+				top.setY(0);
+			}
+			if (getModel().isOpenBottom) {
+				bottom.setWidth(StandaloneSettings.roadWidth * shape.getWidth());
+				bottom.setHeight(Math.round(shape.getHeight() / 2));
+				bottom.setX(Math.round((shape.getWidth() - bottom.getWidth()) / 2));
+				bottom.setY(Math.round(shape.getHeight() / 2));
+			}
+			if (getModel().isOpenLeft) {
+				left.setWidth(Math.round(shape.getWidth() / 2));
+				left.setHeight(StandaloneSettings.roadHeight * shape.getHeight());
+				left.setX(0);
+				left.setY(Math.round(((shape.getHeight() - left.getHeight()) / 2)));
+			}
+			if (getModel().isOpenRight) {
+				right.setWidth(Math.round(shape.getWidth() / 2));
+				right.setHeight(StandaloneSettings.roadHeight * shape.getHeight());
+				right.setX(shape.getWidth() / 2);
+				right.setY(Math.round((shape.getHeight() - right.getHeight()) / 2));
 			}
 		}
 	}
